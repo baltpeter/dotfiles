1b3cbf843c0b9df609b34c33b6d2055678836a08
Benjamin Altpeter <benjamin@oldpeter.de>
2020-09-28 00:36:54 +0200

    Zsh: Support 32-bit prefixes in wine_create_prefix

--- .zsh/functions/wine_create_prefix.zsh
+++ .zsh/functions/wine_create_prefix.zsh
@@ -1,7 +1,24 @@
 wine_create_prefix() {
-    PREFIX="/home/benni/wine-prefixes/$1"
-    cp -R '/home/benni/wine-prefixes/_template64' "$PREFIX";
-    export WINEARCH=win64;
-    export WINEPREFIX=$PREFIX;
-    open $PREFIX;
+    if (( ARGC < 1 )); then
+        printf 'usage: wine_create_prefix <prefix_name> [<arch>]\n\narguments:\n  <arch>        The Wine architecture (32 or 64) [default: 64].\n'
+        return 1;
+    fi
+
+    local PREFIX_NAME=$1
+    local ARCH=$2
+
+    if [ -z "$ARCH" ]; then
+        ARCH=64
+    fi
+
+    case $ARCH in
+        32|64) ;;
+        *) echo 'Only 32 or 64 are allowed as <arch>.\n'; return 1; ;;
+    esac
+
+    local PREFIX="/home/benni/wine-prefixes/$PREFIX_NAME"
+    cp -R "/home/benni/wine-prefixes/_template$ARCH" "$PREFIX";
+    export WINEARCH="win$ARCH";
+    export WINEPREFIX="$PREFIX";
+    open "$PREFIX";
 }
